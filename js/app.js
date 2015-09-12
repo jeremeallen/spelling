824196792e1e5d617da8aaf8424b5c6aedbd4817
Jereme Allen <jereme.allen@wheniwork.com>
2015-09-12 13:33:52 -0500

    Implement basic authentication and security

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var spelling_app = new Vue({
     el: '#app',
     data: {
+        logged_in: false,
+        logged_in_email: '',
         number_correct: 0,
         spelling_words: [],
         error_message: ''
@@ -44,17 +46,85 @@ var spelling_app = new Vue({
             }
 
             this.updateNumberCorrect();
+        },
+        login: function(e) {
+            e.preventDefault();
+
+            app.ref.authWithPassword({
+                email    : $("#inputEmail").val(),
+                password : $("#inputPassword").val()
+            }, app.authHandler);
+        },
+        logout: function(e) {
+            e.preventDefault();
+            app.authLogout();
+            this.logged_in = false;
+            this.logged_in_email = '';
         }
+    },
+    beforeCompile: function() {
+        
     }
 });
 
-(function(){
-    var myFirebaseRef = new Firebase('http://spelling-test.firebaseio.com');
-    var wordsRef = myFirebaseRef.child("words");
+var app = {
+    ref: null,
+    words_ref: null,
+    spelling_app: null,
 
-    wordsRef.on("value", function(snapshot) {
-        spelling_app.$data.spelling_words = snapshot.val();
-    }, function (errorObject) {
-        spelling_app.$data.error_message = "The read failed: " + errorObject.code;
-    });
+    init: function(application) {
+        this.ref = new Firebase('http://spelling-test.firebaseio.com');
+        this.words_ref = this.ref.child("words");
+        this.spelling_app = application;
+
+        this.wireEvents();
+    },
+
+    wireEvents: function() {
+
+        //Get the words from the API
+        this.words_ref.on("value", function(snapshot) {
+            this.spelling_app.$data.spelling_words = snapshot.val();
+        }, function (errorObject) {
+            this.spelling_app.$data.error_message = "The read failed: " + errorObject.code;
+        });
+
+        //Authentication
+        this.ref.onAuth(this.authDataCallback);
+    },
+
+    authDataCallback: function(authData) {
+        if (authData) {
+            console.log("User " + authData.uid + " is logged in with " + authData.provider);
+            this.spelling_app.$data.logged_in = true;
+            this.spelling_app.$data.logged_in_email = authData.password.email;
+        } else {
+            console.log("User is logged out");
+            this.spelling_app.$data.logged_in = false;
+        }
+    },
+
+    authHandler: function(error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+            this.spelling_app.$data.error_message = error;
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+            this.spelling_app.$data.logged_in = true;
+            this.spelling_app.$data.error_message = "";
+        }
+    },
+
+    authLogout: function() {
+        this.ref.unauth();
+    }
+};
+
+(function(){
+    "use strict";
+
+    //Fire off the app
+    var a = app;
+    a.init(spelling_app);
+
 })();
